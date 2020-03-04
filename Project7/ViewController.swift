8ac2210a121b345293ee79cdc6169a4dceb24b59
Zack <ishaqamincoop@icloud.com>
2020-03-04 10:02:43 +0000

    First commit new project

---
+++ Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Ishaq Amin on 04/03/2020.
+//  Copyright © 2020 Ishaq Amin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
